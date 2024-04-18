de1acc72bf4ec838e69d73f937c5df168c3db8ac
Master <128065303+BAoD1nH@users.noreply.github.com>
2024-04-18 10:45:03 +0700

    add prototype, ipynb file

---
+++ prototype.py
@@ -0,0 +1,27 @@
+
+
+#A logical variable is assigned to each cell of the matrix 
+#Trap = True, Gem = False, Others = Number
+def readInput(fileName):
+
+#Write constraints for cells containing numbers
+#To obtain a set of constraint clauses in CNF
+#Generate CNFs automatically.
+def generateCNF():
+
+#Using the pysat library to find the value for each variable
+# Infer the result
+def solveMapPysat():
+
+# Choose and apply an optimal algorithm to solve the CNF.
+def solveMapOptimal():
+
+#Measuring running time
+def bruteForce():
+
+
+def backTracking():
+
+#Ham phu
+def printCurrentMap():
+
