a1b5c683f8103e22b49d6dc0beefc3b70bff5bbb
Nguyen Cong Tuan <nguyencongtuan0810@gmail.com>
2024-04-27 17:04:50 +0700

    Update BruteForce

--- prototype.py
+++ prototype.py
@@ -40,7 +40,7 @@ class Map(Cell):
             if (self.map_array[i][j].value == None):
               color = "purple"
             elif (self.map_array[i][j].value == "T"):
-              color = "rosybrown"
+              color = "red"
             elif (self.map_array[i][j].value == "G"):
               color = "gold"
 
@@ -68,29 +68,133 @@ class Map(Cell):
 #To obtain a set of constraint clauses in CNF
 #Generate CNFs automatically.
 def generateCNF():
-
+    pass
 #Using the pysat library to find the value for each variable
 # Infer the result
 def solveMapPysat():
-
+    pass
 # Choose and apply an optimal algorithm to solve the CNF.
 def solveMapOptimal():
+    pass
 
 #Measuring running time
-def bruteForce():
+def bruteForce(map):
+    size = map.size
+    
+    def is_valid_configuration(configuration):
+        # Check if the configuration satisfies all constraints
+        for i in range(size):
+            for j in range(size):
+                cell = map.map_array[i][j]
+                if cell.value is not None:
+                    count_traps = sum(1 for ni, nj in neighbors(i, j) if configuration[ni][nj] == 'T')
+                    if cell.value != count_traps:
+                        return False
+        return True
+    
+    def generate_configurations(configuration, idx):
+        if idx == size * size:
+            return is_valid_configuration(configuration)
+        
+        i, j = divmod(idx, size)
+        
+        if map.map_array[i][j].value is None:
+            configuration[i][j] = 'T'
+            if generate_configurations(configuration, idx + 1):
+                return True
+            configuration[i][j] = 'G'
+            if generate_configurations(configuration, idx + 1):
+                return True
+            return False
+        else:
+            return generate_configurations(configuration, idx + 1)
+    
+    def neighbors(i, j):
+        for ni in range(max(0, i-1), min(size, i+2)):
+            for nj in range(max(0, j-1), min(size, j+2)):
+                if ni != i or nj != j:
+                    yield ni, nj
+    
+    initial_configuration = [[map.map_array[i][j].value for i in range(size)] for j in range(size)]
+    
+    # Generate configurations recursively
+    result = generate_configurations(initial_configuration, 0)
+    
+    if result:
+        for i in range(size):
+            for j in range(size):
+                if initial_configuration[i][j] == 'T':
+                    map.map_array[i][j].value = "T"
+                elif initial_configuration[i][j] == 'G':
+                    map.map_array[i][j].value = "G"
+                else:
+                    map.map_array[i][j].value = map.map_array[i][j].value
+        map.printMap()
+    else:
+        print("No solution found.")
 
-
-def backTracking():
+def backtracking(map):
+    size = map.size
+    
+    def is_valid_configuration(configuration):
+        # Check if the configuration satisfies all constraints
+        for i in range(size):
+            for j in range(size):
+                cell = map.map_array[i][j]
+                if cell.value is not None:
+                    count_traps = sum(1 for ni, nj in neighbors(i, j) if configuration[ni][nj] == 'T')
+                    if cell.value != count_traps:
+                        return False
+        return True
+    
+    def generate_configurations(configuration, idx):
+        if idx == size * size:
+            return is_valid_configuration(configuration)
+        
+        i, j = divmod(idx, size)
+        
+        if map.map_array[i][j].value is None:
+            for value in ['T', 'G']:
+                configuration[i][j] = value
+                if generate_configurations(configuration, idx + 1):
+                    return True
+            return False
+        else:
+            return generate_configurations(configuration, idx + 1)
+    
+    def neighbors(i, j):
+        for ni in range(max(0, i-1), min(size, i+2)):
+            for nj in range(max(0, j-1), min(size, j+2)):
+                if ni != i or nj != j:
+                    yield ni, nj
+    
+    initial_configuration = [['_' for _ in range(size)] for _ in range(size)]
+    
+    # Generate configurations recursively
+    result = generate_configurations(initial_configuration, 0)
+    
+    if result:
+        for i in range(size):
+            for j in range(size):
+                if initial_configuration[i][j] == 'T':
+                    map.map_array[i][j].value = "T"
+                else:
+                    map.map_array[i][j].value = "G"
+        map.printMap()
+    else:
+        print("No solution found.")
 
 #Ham phu
 def printCurrentMap():
-
+    pass
 
 
 
 # Main
-file_path = "Map/9x9/9x9.txt"
+file_path = "Map/5x5/5x5.txt"
 map = Map()
-map.readFile(file_path)
+map.readInput(file_path)
 map.printMap()
 
+bruteForce(map)
+backtracking(map)
